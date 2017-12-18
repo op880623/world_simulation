84118c930ccabaed97259975ea406ef53e407e8e
op880623 <op880623@gmail.com>
2017-12-18 16:48:08 +0800

    add content of Grass object

--- life/plant/grass.rb
+++ life/plant/grass.rb
@@ -1,1 +1,33 @@
 require_relative "plant.rb"
+
+class Grass < Plant
+
+  @@grass = []
+
+  def initialize(lng, lat)
+    super(lng, lat)
+    @@grass.push(self)
+  end
+
+  def self.count()
+    return @@grass.size
+  end
+
+  def self.all()
+    return @@grass
+  end
+
+  def self.get(lng, lat)
+    set = []
+    for grass in @@grass
+       set.push(grass) if grass.lng == lng && grass.lat == lat
+    end
+    return set
+  end
+
+  def die()
+    @@grass.delete(self)
+    super()
+  end
+
+end
