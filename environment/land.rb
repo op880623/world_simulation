1fcea39dcbd2fa33481bb82a897e9055894623b0
op880623 <op880623@gmail.com>
2017-12-25 10:54:53 +0800

    completed basic Land object

--- environment/land.rb
+++ environment/land.rb
@@ -4,4 +4,45 @@ class Land < Environment
 
   @@lands = []
 
+  def initialize(location)
+    super(location)
+    @waterMax = rand(0..10000)
+    @water = @waterMax
+    @@lands.push(self)
+  end
+
+  def self.count()
+    return @@lands.size
+  end
+
+  def self.all()
+    return @@lands
+  end
+
+  def destroy()
+    super()
+    @@lands.delete(self)
+  end
+
+  def water_max()
+    return @waterMax
+  end
+
+  def water()
+    return @water
+  end
+
+  def water_used(quantity)
+    if quantity <= @water
+      @water -= quantity
+      return quantity
+    else
+      return nil
+    end
+  end
+
+  def restore()
+    @water = self.water_max()
+  end
+
 end
